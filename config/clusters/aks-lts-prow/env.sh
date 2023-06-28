8d73ac1655a4790ba3d273ed38e98d4c11ebb1b1
Max Horstmann <maxhorstmann@microsoft.com>
2023-06-28 11:13:01 -0400

    remove shell script

--- config/clusters/aks-lts-prow/env.sh
+++
@@ -1,8 +0,0 @@
-export GITHUB_ORG=MaxHorstmann
-export GITHUB_APP_ID=352626
-export PROW_HOST=prow.example.com
-export AZURE_STORAGE_ACCOUNT_USER=todo
-export AZURE_STORAGE_ACCOUNT_PASSWORD=todo
-export GITHUB_TOKEN=todo
-export HMAC_TOKEN=todo
-export MINIO_CONSOLE_PORT=80
